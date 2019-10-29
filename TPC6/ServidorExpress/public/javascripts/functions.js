ecfe33f7e657dcd58444b333c3eeacd1e2130a89
Eddy32 <pedroritolima@gmail.com>
2019-10-29 09:11:53 +0000

    DONE

--- TPC6/ServidorExpress/public/javascripts/functions.js
+++
@@ -1,8 +0,0 @@
-
-function apagaItem(ident){
-    console.log("vou tentar apagar o " + ident + "... :(")
-    axios.delete('/' + ident)
-        .then(response => window.location.assign('/'))
-        .catch(error => console.log(error))
-}
-
