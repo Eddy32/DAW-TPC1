d2b1760c93413482bfcf9383f9fa609ffab44ebd
Eddy32 <pedroritolima@gmail.com>
2019-11-05 21:23:40 +0000

    DONE

---
+++ TPC7/GestorFilmes/public/javascripts/operacoes.js
@@ -0,0 +1,35 @@
+// Apagar um filme 
+function eliminar(id) {
+    axios.delete("/filmes/" + id)
+        .then(response => window.location.assign("/"))
+        .catch(err => console.log(err))
+}
+
+// Atualizar um filme
+function atualizar(id) {
+    var inputs = document.getElementsByClassName( 'names' ),
+    atores  = [].map.call(inputs, function( input ) {
+        return input.value;
+    });
+
+    var inputs = document.getElementsByClassName( 'genres' ),
+    generos  = [].map.call(inputs, function( input ) {
+        return input.value;
+    });
+    
+    
+    console.log("AQUI: " + names + "AJJA" + generos)
+
+    var newFilm = {
+
+        title: document.getElementById("title").value,
+        year: document.getElementById("year").value,
+        cast: names,
+        genres: generos
+
+    }
+
+    axios.put("/filmes/" + id, newFilm)
+        .then(response => window.location.assign("/filmes/" + id))
+        .catch(err => console.log(err))
+}
